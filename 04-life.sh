c535e34623181c08f1fa6e69e3ab3851c4d0d5a0
woodyum <vum@ggc.edu>
2024-04-28 11:56:33 -0400

    first change

--- 04-life.sh
+++ 04-life.sh
@@ -6,7 +6,7 @@ read meaning
 if [ "$meaning" -eq 42 ]; then
    echo "Yes!, That is the meaning of life!"
 else
-   echo "Awww... You don't know the meaning of life"
+   echo "I don't know the meaning of life"
 fi
 
 #  here are some other arithemetic comparison operators
